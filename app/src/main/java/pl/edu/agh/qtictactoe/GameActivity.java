4a46fac08f182c9433e2dd5b20b7d9c5ad7461be
AMaczynski <artur.m1717@gmail.com>
2019-10-22 19:38:58 +0200

    demo of game layout

---
+++ app/src/main/java/pl/edu/agh/qtictactoe/GameActivity.java
@@ -0,0 +1,19 @@
+package pl.edu.agh.qtictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+    }
+}
