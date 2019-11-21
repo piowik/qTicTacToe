75666a0b86122d3f841f73139aff933fea30485e
AMaczynski <artur.m1717@gmail.com>
2019-11-21 20:16:18 +0100

    more tests

--- app/src/test/java/pl/edu/agh/qtictactoe/logic/GameLogicTest.java
+++ app/src/test/java/pl/edu/agh/qtictactoe/logic/GameLogicTest.java
@@ -1,6 +1,5 @@
 package pl.edu.agh.qtictactoe.logic;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,15 +8,15 @@ import java.util.List;
 import pl.edu.agh.qtictactoe.model.GameState;
 import pl.edu.agh.qtictactoe.model.Move;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
 public class GameLogicTest {
 
-    GameState gameState = new GameState();
-
-    @Before
-    public void init() {
+    @Test
+    public void testIsQuantumLoopAssertTrue1() {
+        GameState gameState = new GameState();
         List<Move> movesX = new ArrayList<>(4);
         movesX.add(new Move(1, 1, 2));
         movesX.add(new Move(3, 1, 3));
@@ -31,13 +30,45 @@ public class GameLogicTest {
         movesO.add(new Move(6, 2, 7));
         movesO.add(new Move(8, 4, 9));
         gameState.setMovesO(movesO);
-    }
 
-    @Test
-    public void testIsQuantumLoop() {
         GameLogic logic = new GameLogic(gameState);
         Move lastMove = new Move(7, 4, 9);
         boolean isQuantumLoop = logic.isQuantumLoop(3, lastMove, 7);
         assertTrue("WHAT TO WRITE HERE?", isQuantumLoop);
+    }
+
+    @Test
+    public void testIsQuantumLoopAssertTrue2() {
+        GameState gameState = new GameState();
+        List<Move> movesX = new ArrayList<>(4);
+        movesX.add(new Move(1, 1, 2));
+
+        gameState.setMovesX(movesX);
+
+        List<Move> movesO = new ArrayList<>(4);
+        movesO.add(new Move(2, 1, 2));
+
+
+        gameState.setMovesO(movesO);
+
+        GameLogic logic = new GameLogic(gameState);
+        Move lastMove = new Move(2, 1, 2);
+        boolean isQuantumLoop = logic.isQuantumLoop(1, lastMove, 2);
+        assertTrue("WHAT TO WRITE HERE?", isQuantumLoop);
+    }
+
+    @Test
+    public void testIsQuantumLoopAssertFalse() {
+        GameState gameState = new GameState();
+        List<Move> movesX = new ArrayList<>(1);
+        movesX.add(new Move(1, 1, 2));
+        gameState.setMovesX(movesX);
+        List<Move> movesO = new ArrayList<>(0);
+        gameState.setMovesO(movesO);
+
+        GameLogic logic = new GameLogic(gameState);
+        Move lastMove = new Move(1, 1, 2);
+        boolean isQuantumLoop = logic.isQuantumLoop(1, lastMove, 2);
+        assertFalse("WHAT TO WRITE HERE?", isQuantumLoop);
     }
 }
